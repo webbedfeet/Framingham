a634d7e46f2fcf8fb68c61fdbbc2a1319daf77fd
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-11-13 11:15:07 -0500

    Added comorbidity figures

--- IncidenceSummaries.R
+++ IncidenceSummaries.R
@@ -7,6 +7,7 @@
 #+ preamble, include=F
 ProjTemplate::reload()
 library(glue)
+knitr::opts_chunk$set(cache = T, echo = F)
 load('data/rda/predictors.rda')
 
 #' We need to compute the person-years at risk in each 5 year period, and see if the incidence rates
@@ -31,7 +32,7 @@ py_off <- dat_offspring %>% group_by(yr_grp, age_grp) %>%
   ungroup() %>%
   filter(!is.na(yr_grp), !is.na(age_grp))
 
-dat_overall <- rbind(dat_orig %>% select(-bmi), dat_offspring)
+dat_overall <- rbind(dat_orig, dat_offspring)
 
 py_overall <- dat_overall %>%
   group_by(age_grp, yr_grp) %>%
@@ -75,16 +76,58 @@ py_off %>% filter(age_grp != '(0,40]', py > 100, !is.na(yr_grp), !is.na(age_grp)
   labs(x = 'Year', y = 'Incidence rate (per 1000 py)')+
   ggtitle('Offspring cohort')
 
-#+ echo = F, fig.height = 7, fig.width=11
+#+ echo = F, fig.height = 4, fig.width=11
 py_overall %>% filter(age_grp != '(0,40]', py > 100, !is.na(yr_grp), !is.na(age_grp)) %>%
   mutate(yr_grp = fct_relabel(yr_grp, fix_grps),
          age_grp = fct_relabel(age_grp, fix_grps)) %>%
   ggplot(aes(x = yr_grp, y = inc_rate)) + geom_point() +
   stat_summary(fun.y = 'mean', geom = 'line', mapping = aes(group = age_grp))+
-  facet_wrap(~age_grp, scales = 'free_y') +
+  facet_wrap(~age_grp, scales = 'free_y', nrow = 1) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(x = 'Year', y = 'Incidence rate (per 1000 py)')+
   ggtitle('Combined cohorts')
 
+#' # Comorbidities
+#'
+#+ include=F
+comorb_orig <- dat_orig %>% group_by(age_grp, yr_grp) %>%
+  summarize_at(vars(diab:rf_etoh), mean, na.rm=T) %>%
+  filter(age_grp != '(0,40]', !is.na(yr_grp), !is.na(age_grp)) %>%
+  left_join(dat_orig %>% count(age_grp, yr_grp)) %>% ungroup()
+comorb_off <- dat_offspring %>% group_by(age_grp, yr_grp) %>%
+  summarize_at(vars(diab:bmi), mean, na.rm=T) %>%
+  filter(age_grp != '(0,40]', !is.na(yr_grp), !is.na(age_grp)) %>%
+  left_join(dat_offspring %>% count(age_grp, yr_grp)) %>% ungroup()
 
+comorb_combined <- list('original' = comorb_orig, 'offspring' = comorb_off) %>%
+  bind_rows(.id = 'cohort') %>%
+  mutate(cohort = fct_relevel(cohort, 'original'),
+         yr_grp = factor(yr_grp)) %>%
+  mutate_at(vars(contains('grp')), fix_grps)
 
+#' ## Diabetes
+#+ echo = F
+ggplot(comorb_combined, aes(yr_grp, diab*100))+geom_point()+facet_grid(cohort ~ age_grp)+
+  labs(y = 'Diabetes', x = 'Year')+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#' ## Heavy Drinking
+#+ echo = F
+ggplot(comorb_combined, aes(yr_grp, rf_etoh*100))+geom_point()+facet_grid(cohort ~ age_grp)+
+  labs(y = 'Heavy Drinking', x = 'Year')+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#' ## Smoking
+#+ echo = F
+ggplot(comorb_combined, aes(yr_grp, smoking*100))+geom_point()+facet_grid(cohort ~ age_grp)+
+labs(y = 'Smoking', x = 'Year')+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#' ## BMI
+#+ echo = F
+ggplot(comorb_combined, aes(yr_grp, bmi))+geom_point()+facet_grid(cohort ~ age_grp)+
+  labs(y = 'BMI', x = 'Year')+
+  geom_hline(yintercept = c(25,30), color='red',linetype=2)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#' __Caveat__: BMI from offspring cohort is based on one recorded BMI at exam 2.
